e0c0b4d4d542f77839eecf13dbe7eaaa99e682a2
Harveer <106449958+harveerd123@users.noreply.github.com>
2025-12-08 16:55:15 +0000

    Add project files.

---
+++ myfirstvuetsproject/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import ToDoWidget from './components/ToDoWidget.vue'
+  import NotesWidget from './components/NotesWidget.vue'
+  import QuoteWidget from './components/QuoteWidget.vue'
+  import WidgetContainer from './components/WidgetContainer.vue'
+</script>
+
+<template>
+  <div class="dashboard">
+    <WidgetContainer title="To-Do List">
+      <ToDoWidget />
+    </WidgetContainer>
+
+    <WidgetContainer title="Notes">
+      <NotesWidget />
+    </WidgetContainer>
+
+    <WidgetContainer title="Daily Quote">
+      <QuoteWidget />
+    </WidgetContainer>
+  </div>
+</template>
+
---
+++ myfirstvuetsproject/src/components/WidgetContainer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <div class="widget">
+    <h2>{{title}}</h2>
+    <div><slot /></div>
+  </div>
+</template>
+
+<style scoped>
+.widget{
+    background: white;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 2px 8px rgba(0,0,0,.1);
+}
+</style>
