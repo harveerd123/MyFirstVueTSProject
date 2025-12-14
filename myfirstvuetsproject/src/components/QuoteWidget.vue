0e53854a2ffd8b2619445017ade26d5ea73c7c9d
Harveer <106449958+harveerd123@users.noreply.github.com>
2025-12-14 17:45:09 +0000

    set up quote page

--- myfirstvuetsproject/src/components/QuoteWidget.vue
+++ myfirstvuetsproject/src/components/QuoteWidget.vue
@@ -3,6 +3,42 @@
 </script>
 
 <template>
-
+  <div>
+    <p class="quote">{{quote}}</p>
+    <button @click="newQuote">New Quote</button>
+  </div>
 </template>
 
+
+<script setup>
+  import { ref } from 'vue'
+
+  const quotes = [
+    "Success is not final; failure is not fatal.",
+    "Do something today that your future self will thank you for.",
+    "The only limit is your mind.",
+    "Small steps every day."
+  ]
+
+  const quote = ref(localStorage.getItem('dailyQuote') || random())
+
+  function random() {
+    return quotes[Math.floor(Math.random() * quotes.length)]
+  }
+
+  function newQuote() {
+    quote.value = random()
+    localStorage.setItem('dailyQuote', quote.value)
+  }
+</script>
+
+<style scoped>
+  .quote {
+    font-style: italic;
+    margin-bottom: 15px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+</style>
