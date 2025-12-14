6e96f5fb02c50a8558c7c4317af0bd39c7ebf396
Harveer <106449958+harveerd123@users.noreply.github.com>
2025-12-14 17:42:31 +0000

    set up notes page

--- myfirstvuetsproject/src/components/NotesWidget.vue
+++ myfirstvuetsproject/src/components/NotesWidget.vue
@@ -3,6 +3,28 @@
 </script>
 
 <template>
-  
+<div>
+  <textarea v-model="notes" @input="save" placeholder="Write notes here..."></textarea>
+</div>
 </template>
 
+<script setup>
+  import { ref } from 'vue'
+
+  const notes = ref(localStorage.getItem('notes') || '')
+
+  const save = () => {
+    localStorage.setItem('notes', notes.value);
+  }
+</script>
+
+<style scoped>
+  textarea {
+    width: 100%;
+    height: 200px;
+    padding: 10px;
+    border-radius: 6px;
+    border: 1px solid #ccc;
+    resize: vertical;
+  }
+</style>
